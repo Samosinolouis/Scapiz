1b35773d4dda851be4e17906a8a90c020baaa93c
Louis Jefferson Samosino <68355394+Samosinolouis@users.noreply.github.com>
2024-05-12 01:58:36 +0800

    Add files via upload

---
+++ preview.js
@@ -0,0 +1,28 @@
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+function renderPDF(url, containerId) {
+  pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+    var pdfContainer = document.getElementById(containerId);
+    var pageNum = 1;
+    var scale = 1.5;
+
+    pdfDoc_.getPage(pageNum).then(function(page) {
+      var viewport = page.getViewport({scale: scale});
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext('2d');
+      var renderContext = {
+        canvasContext: ctx,
+        viewport: viewport
+      };
+
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      pdfContainer.appendChild(canvas);
+
+      page.render(renderContext);
+    });
+  });
+}
+
+renderPDF('upload/ICT/SAMOSINO_RESLIFE1-ACTIVITY5.pdf', 'pdfContainer1');
+renderPDF('upload/ICT/PORTILLO_RESLIFE1-ACTIVITY5.pdf', 'pdfContainer2');
